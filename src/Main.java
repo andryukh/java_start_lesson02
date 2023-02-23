308423257835348a5c1b4f7f02d1dca279b9db3b
Yukha <andr2350@gmail.com>
2023-02-23 14:44:37 +0300

    ДЗ: добавил деление. Факултативно добавил защиту от деления на ноль.

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,31 @@ public class Main {
         System.out.println("1. Сложение");
         System.out.println("2. Вычитание");
         System.out.println("3. Умножение");
+        System.out.println("4. Деление");
         Scanner scanner = new Scanner(System.in);
+
         int operation = scanner.nextInt();
 
         System.out.println("Введите первое число:");
-        int a = scanner.nextInt();
-
-        System.out.println("Введите второе число:");
-        int b = scanner.nextInt();
-
-        int result;
+        float a = scanner.nextInt();
+        float b;
+        do {
+            System.out.println("Введите второе число:");
+            b = scanner.nextInt();
+            if (b == 0) {
+                System.out.println("На ноль нельзя делить!!1");
+            }
+        } while (b == 0);
+        float result;
 
         if (operation == 1) {
             result = a + b;
         } else if (operation == 2) {
             result = a - b;
-        } else {
+        } else if (operation == 3) {
             result = a * b;
+        } else {
+            result = a / b;
         }
 
         System.out.println("Результат = " + result);
